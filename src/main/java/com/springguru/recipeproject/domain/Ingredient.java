bdec938eea5ef2fd3d507b5e308bd0b6967f40b8
Shamoiev <wamoev@gmail.com>
2021-03-13 22:10:00 +0200

    #jpa one-to-many and many-to-one relations

---
+++ src/main/java/com/springguru/recipeproject/domain/Ingredient.java
@@ -0,0 +1,48 @@
+package com.springguru.recipeproject.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String description;
+    BigDecimal amount;
+    //    private UnitOfMeasure unitOfMeasure;
+    @ManyToOne
+    private Recipe recipe;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
